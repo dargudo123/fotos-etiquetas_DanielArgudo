ade903cf2991e53771ee6af208b65d962dc4285c
dargudo123 <dargudo123@gmail.com>
2025-10-14 17:45:56 -0500

    Primer commit - proyecto fotos-etiquetas

---
+++ app/Models/Photo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Photo extends Model {
+    use HasFactory;
+    protected $fillable = ['title', 'url'];
+    public function tags() {
+        return $this->belongsToMany(Tag::class); 
+    }
+}
